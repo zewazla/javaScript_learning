832c9b9d08583370d9ca0d8e931bfc09ef62c3eb
Bożka Majchrzak <zewazla@gmail.com>
2021-01-17 22:53:58 +0100

    simple button/click/showtext/refresh

---
+++ simple_button/button_script.js
@@ -0,0 +1,10 @@
+function Click() {
+
+    document.getElementById("text1").innerHTML = "Hellow";
+    document.getElementById("text2").innerHTML = "How are you?";
+
+}
+
+function refreshPage() {
+    window.location.reload();
+}
